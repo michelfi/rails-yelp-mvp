f1119f5271cf0e90096f919afd6c72b072da085b
michelfi <michel.figueres@gmail.com>
2023-08-23 16:37:19 +0200

    exo pas terminé

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,58 @@
+# class RestaurantsController < ApplicationController
+#   def index
+#     @restaurants = Restaurant.all
+#   end
+
+#   def show
+#   end
+
+#   def new
+#     @restaurants = Restaurant.new
+#   end
+
+#   def create
+#     @restaurant = Restaurant.new(restaurant_params)
+
+#     if @restaurant.save
+#       redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+#     else
+#       render :new
+#     end
+#   end
+
+# end
+
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destroy_all
+
+restaurants = [
+  { name: "resto chinois" }, { address: "Lord of the Rings" }, { phone_number: "06 12 13 15 65"}, { category: "chinese" },
+  { name: "resto italen" }, { address: "27 rue des alouettes" }, { phone_number: "06 99 13 15 65" }, { category: "italian" },
+  { name: "resto japonais" }, { address: "15 rue de la victoire" }, { phone_number: "06 12 13 15 74" }, { category: "japanese" },
+  { name: "resto français" }, { address: "13 rue turenne" }, { phone_number: "06 12 82 15 65" }, { category: "french" },
+  { name: "resto belge" }, { address: "55 route des guerriers" }, { phone_number: "06 92 13 15 65" }, { category: "belgian" }]
+
+  restaurants.each do |data|
+    Restaurant.create(
+      name: data[:name],
+      address: data[:address],
+      phone_number: data[:phone_number],
+      category: data[:category]
+    )
+  end
